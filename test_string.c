93095c89fd30ed93022af1d3970310f88a677983
sfeek <shane.feek@gmail.com>
2018-07-18 08:23:25 -0400

    Updated error handling

--- test_string.c
+++ test_string.c
@@ -3,39 +3,48 @@
 #include <unistd.h>
 #include "simplestring.h"
 
+#define SUCCESS 0
+#define FAIL 1
+
 int main(void)
 {
-	char *s1=NULL;
-	char *s2=NULL;
+	int error = SUCCESS;
+	char *s1 = NULL;
+	char *s2 = NULL;
 
 	if (safe_copy(&s1,"This is a"))
 	{
 		printf("Copy error occurred\n");
-		return 1;
+		error = FAIL;
+		goto main_cleanup;
 	}
 
 	if (safe_copy(&s2," Here is another sentence for the end."))
 	{
 		printf("Copy error occurred\n");
-		return 1;
+		error = FAIL;
+		goto main_cleanup;
 	}
 
 	if (safe_append(&s1," test of my program!"))
 	{
 		printf("Concat error occurred\n");
-		return 1;
+		error = FAIL;
+		goto main_cleanup;
 	}
 	
 	if (safe_append(&s1," I hope that it works fine this way."))
 	{
 		printf("Concat error occurred\n");
-		return 1;
+		error = FAIL;
+		goto main_cleanup;
 	}
 
 	if (safe_append(&s1,s2))
 	{
 		printf("Concat error occurred\n");
-		return 1;
+		error = FAIL;
+		goto main_cleanup;
 	}
 
 	printf("The concat string: %s\n",s1);
@@ -43,22 +52,24 @@ int main(void)
 	if (safe_sprintf(&s1,"%s","This is sprintf!\n"))
 	{
 		printf("Sprintf error occurred\n");
-		return 1;
+		error = FAIL;
+		goto main_cleanup;
 	}
 
 	printf("%s",s1);
-	sleep (5);
 
 	if (safe_truncate(&s1,5))
 	{
 		printf("Truncate error occurred\n");
-		return 1;
+		error = FAIL;
+		goto main_cleanup;
 	}
 	
 	printf("The truncated string: %s\n", s1);
 	
+	main_cleanup:
 	free(s1);
 	free(s2);
 	
-return 0;
+	return error;
 }
