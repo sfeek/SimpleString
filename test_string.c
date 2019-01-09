0f66f01a6163552fe8e569ebc0d90b18c0f62b6a
sfeek <shane.feek@gmail.com>
2019-01-09 10:10:15 -0500

    Linted

--- test_string.c
+++ test_string.c
@@ -6,70 +6,69 @@
 #define SUCCESS 0
 #define FAIL 1
 
-int main(void)
+int main (void)
 {
 	int error = SUCCESS;
 	char *s1 = NULL;
 	char *s2 = NULL;
 
-	if (safe_copy(&s1,"This is a"))
+	if (safe_copy (&s1, "This is a"))
 	{
-		printf("Copy error occurred\n");
+		printf ("Copy error occurred\n");
 		error = FAIL;
 		goto main_cleanup;
 	}
 
-	if (safe_copy(&s2," Here is another sentence for the end."))
+	if (safe_copy (&s2, " Here is another sentence for the end."))
 	{
-		printf("Copy error occurred\n");
+		printf ("Copy error occurred\n");
 		error = FAIL;
 		goto main_cleanup;
 	}
 
-	if (safe_append(&s1," test of my program!"))
+	if (safe_append (&s1, " test of my program!"))
 	{
-		printf("Concat error occurred\n");
-		error = FAIL;
-		goto main_cleanup;
-	}
-	
-	if (safe_append(&s1," I hope that it works fine this way."))
-	{
-		printf("Concat error occurred\n");
+		printf ("Concat error occurred\n");
 		error = FAIL;
 		goto main_cleanup;
 	}
 
-	if (safe_append(&s1,s2))
+	if (safe_append (&s1, " I hope that it works fine this way."))
 	{
-		printf("Concat error occurred\n");
+		printf ("Concat error occurred\n");
 		error = FAIL;
 		goto main_cleanup;
 	}
 
-	printf("The concat string: %s\n",s1);
-
-	if (safe_sprintf(&s1,"%s","This is sprintf!\n"))
+	if (safe_append (&s1, s2))
 	{
-		printf("Sprintf error occurred\n");
+		printf ("Concat error occurred\n");
 		error = FAIL;
 		goto main_cleanup;
 	}
 
-	printf("%s",s1);
+	printf ("The concat string: %s\n", s1);
 
-	if (safe_truncate(&s1,5))
+	if (safe_sprintf (&s1, "%s", "This is sprintf!\n"))
 	{
-		printf("Truncate error occurred\n");
+		printf ("Sprintf error occurred\n");
 		error = FAIL;
 		goto main_cleanup;
 	}
-	
-	printf("The truncated string: %s\n", s1);
-	
-	main_cleanup:
-	free(s1);
-	free(s2);
-	
+
+	printf ("%s", s1);
+
+	if (safe_truncate (&s1, 5))
+	{
+		printf ("Truncate error occurred\n");
+		error = FAIL;
+		goto main_cleanup;
+	}
+
+	printf ("The truncated string: %s\n", s1);
+
+main_cleanup:
+	free (s1);
+	free (s2);
 	return error;
 }
